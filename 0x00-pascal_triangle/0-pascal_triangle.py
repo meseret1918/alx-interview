c8d40e658aad127c2de83cb90bdf35964b433790
meseret1918 <meseret1918@gmail.com>
2024-10-06 13:42:00 +0000

    task update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,11 +2,18 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    triangle = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        if i > 1:
-            for j in range(1, i):
-                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        triangle.append(row)
+
+    triangle = [[1]]  # The first row is always [1]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]  # Previous row
+        new_row = [1]  # Start the row with 1
+
+        # Calculate the values in between
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)  # End the row with 1
+        triangle.append(new_row)  # Add the row to the triangle
+
     return triangle
