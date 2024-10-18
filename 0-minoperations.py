217fea44272c6f80127529671f83d91f5386367b
meseret1918 <meseret1918@gmail.com>
2024-10-19 00:58:02 +0300

    Minimum Operations

---
+++ 0-minoperations.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    n = 4
+    print("Min # of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+    n = 9
+    print("Min # of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+    n = 1
+    print("Min # of operations to reach {} characters: {}".format(n, minOperations(n)))
