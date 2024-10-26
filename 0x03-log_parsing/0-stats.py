6699bbeafd84b6275c6e2b805d78e85efb0b20df
meseret1918 <meseret1918@gmail.com>
2024-10-26 15:54:12 +0300

    interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,54 @@
 #!/usr/bin/python3
-'''A script that generates random HTTP request logs.
-This script simulates HTTP GET requests with random IP addresses,
-timestamps, and status codes.
-'''
+"""
+log parsing
+"""
 
-import random
 import sys
-import datetime
-from time import sleep
+import re
 
 
-def generate_log_entry():
-    '''Generates a single log entry in the HTTP request log format.'''
-    ip = "{:d}.{:d}.{:d}.{:d}".format(
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255)
-    )
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    status_code = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
-    response_size = random.randint(1, 1024)
-
-    log_entry = (
-        f"{ip} - [{timestamp}] \"GET /projects/1216 HTTP/1.1\" "
-        f"{status_code} {response_size}\n"
-    )
-    return log_entry
+def output(log: dict) -> None:
+    """
+    Helper function to display stats.
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
-def main():
-    '''Main function to generate 10,000 log entries.'''
-    for _ in range(10000):
-        sleep(random.uniform(0, 0.1))  # Limit maximum delay to 100 ms
-        sys.stdout.write(generate_log_entry())
-        sys.stdout.flush()
+if __name__ == "__main__":
+    regex = re.compile(
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} '
+        r'\d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" '
+        r'(.{3}) (\d+)'
+    )  # nopep8
 
+    line_count = 0
+    log = {
+        "file_size": 0,
+        "code_frequency": {
+            str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
+        }
+    }
 
-if __name__ == '__main__':
-    main()
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if match:
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # Status code
+                if code.isdecimal():
+                    log["code_frequency"][code] += 1
+
+                if line_count % 10 == 0:
+                    output(log)
+    finally:
+        output(log)
