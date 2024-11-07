fdb666c17df28c1fe1c5af2567505a6a46a10686
meseret1918 <meseret1918@gmail.com>
2024-11-07 12:17:47 +0300

    queen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,50 +1,74 @@
 #!/usr/bin/python3
-""" N queens puzzle, challenge of placing N non attacking queens
-on a NxN chessboard
-This program solves the N queens problem """
-
-from sys import argv
-
-
-def is_NQueen(cell: list) -> bool:
-    """ False if not N Queen, True if N Queen """
-    row_number = len(cell) - 1
-    difference = 0
-    for index in range(0, row_number):
-        difference = cell[index] - cell[row_number]
-        if difference < 0:
-            difference *= -1
-        if difference == 0 or difference == row_number - index:
-            return False
-    return True
+"""
+Solution to the N Queens problem.
+"""
+import sys
 
 
-def solve_NQueens(dimension: int, row: int, cell: list, output: list):
-    """ Return result of N Queens recursively """
-    if row == dimension:
-        print(output)
-    else:
-        for column in range(0, dimension):
-            cell.append(column)
-            output.append([row, column])
-            if (is_NQueen(cell)):
-                solve_NQueens(dimension, row + 1, cell, output)
-            cell.pop()
-            output.pop()
+def backtrack(r, n, cols, pos, neg, board):
+    """
+    Backtracking function to find all solutions for the N Queens problem.
+    
+    Args:
+        r (int): Current row to place the queen.
+        n (int): Size of the chessboard (N x N).
+        cols (set): Set to track columns where queens are placed.
+        pos (set): Set to track positive diagonals where queens are placed.
+        neg (set): Set to track negative diagonals where queens are placed.
+        board (list): List of queen positions for the current solution.
+        
+    This function prints each solution as a list of queen coordinates.
+    """
+    if r == n:
+        print(board)
+        return
+
+    for c in range(n):
+        # Check if the position is safe
+        if c in cols or (r + c) in pos or (r - c) in neg:
+            continue
+
+        # Place queen and add to tracking sets
+        cols.add(c)
+        pos.add(r + c)
+        neg.add(r - c)
+        board.append([r, c])
+
+        # Recur to place the next queen
+        backtrack(r + 1, n, cols, pos, neg, board)
+
+        # Backtrack: remove the queen and update tracking sets
+        cols.remove(c)
+        pos.remove(r + c)
+        neg.remove(r - c)
+        board.pop()
 
 
-if len(argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-try:
-    N = int(argv[1])
-except BaseException:
-    print('N must be a number')
-    exit(1)
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
-else:
-    output = []
-    cell = 0
-    solve_NQueens(int(N), cell, [], output)
+def nqueens(n):
+    """
+    Sets up the board and initiates the backtracking to solve the N Queens problem.
+
+    Args:
+        n (int): The number of queens and the size of the chessboard (N x N).
+    """
+    backtrack(0, n, set(), set(), set(), [])
+
+
+if __name__ == "__main__":
+    # Check for correct number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    # Validate that N is an integer and at least 4
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    # Solve the N Queens problem
+    nqueens(n)
