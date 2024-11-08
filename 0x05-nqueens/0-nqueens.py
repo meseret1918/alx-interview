64f168b3f1d460d99637bc426c0b0ddcab32e3a3
meseret1918 <meseret1918@gmail.com>
2024-11-08 17:16:23 +0000

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,22 +1,53 @@
 #!/usr/bin/python3
-"""
-Solves N queens problem
-"""
 import sys
 
 
-if __name__ == '__main__':
+def print_usage_and_exit(message):
+    """Prints a message and exits the program."""
+    print(message)
+    sys.exit(1)
 
-    if (len(sys.argv) != 2):
-        print('Usage: nqueens N')
-        sys.exit(1)
+
+def solve_nqueens(n):
+    """Solves the N Queens problem and prints all solutions."""
+    solutions = []
+    board = [-1] * n
+
+    def is_safe(row, col):
+        """Checks if a queen can be placed at (row, col) without conflict."""
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
+
+    def place_queen(row):
+        """Tries to place queens row by row and saves solutions."""
+        if row == n:
+            solutions.append(board[:])
+            return
+        for col in range(n):
+            if is_safe(row, col):
+                board[row] = col
+                place_queen(row)
+                board[row] = -1
+
+    place_queen(0)
+    for sol in solutions:
+        print([[i, sol[i]] for i in range(n)])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print_usage_and_exit("Usage: nqueens N")
 
     try:
-        n = int(sys.argv[1])
+        N = int(sys.argv[1])
     except ValueError:
-        print('N must be a number')
-        sys.exit(1)
+        print_usage_and_exit("N must be a number")
 
-    if n < 4:
-        print('N must be at least 4')
-        sys.exit(1)
+    if N < 4:
+        print_usage_and_exit("N must be at least 4")
+
+    solve_nqueens(N)
