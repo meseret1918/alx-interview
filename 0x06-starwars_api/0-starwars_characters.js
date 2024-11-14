d5a3617d821ea6d1c4af8054165afac5231af517
meseret1918 <meseret1918@gmail.com>
2024-11-14 17:36:00 +0300

    starwar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
+
+// Get the movie ID from the command-line argument
 const movieId = process.argv[2];
+
+// Construct the API URL to fetch movie details
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
+// Make a GET request to the Star Wars API
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
 
-  const characters = JSON.parse(body).characters;
+  // Parse the JSON response
+  const movieData = JSON.parse(body);
+  
+  // Extract the characters array
+  const characters = movieData.characters;
+
+  // Iterate through the characters list and fetch character details
   characters.forEach((characterUrl) => {
     request(characterUrl, (error, response, body) => {
       if (error) {
@@ -18,6 +29,7 @@ request(url, (error, response, body) => {
         return;
       }
 
+      // Parse the character's details and log the name
       const character = JSON.parse(body);
       console.log(character.name);
     });
