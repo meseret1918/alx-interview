5e30d7f81348a7840182a62303bd2d474ff28459
meseret1918 <meseret1918@gmail.com>
2024-12-12 21:02:25 -0800

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,61 @@
 #!/usr/bin/python3
-"""Module defining isWinner function."""
+"""
+Prime Game module.
 
+This module implements the logic for determining the winner of the Prime Game.
+"""
 
 def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
+    """
+    Determine the winner of the Prime Game.
 
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
+    Args:
+        x (int): Number of rounds.
+        nums (list): List of integers, where each integer represents the range of numbers for the game.
 
-        if not primesSet:
-            benWinsCount += 1
-            continue
+    Returns:
+        str: Name of the winner ("Maria" or "Ben"), or None if there is no winner.
+    """
+    if x <= 0 or not nums or any(n < 1 for n in nums):
+        return None
 
-        isMariaTurns = True
+    max_num = max(nums)
+    primes = sieve_of_eratosthenes(max_num)
 
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
+    maria_wins = 0
+    ben_wins = 0
 
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
+    for n in nums:
+        prime_count = sum(primes[:n + 1])
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     return None
 
+def sieve_of_eratosthenes(n):
+    """
+    Generate a list of prime indicators up to n using Sieve of Eratosthenes.
 
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
+    Args:
+        n (int): The upper limit to calculate primes.
+
+    Returns:
+        list: A list where primes[i] is True if i is a prime number, otherwise False.
+    """
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
     for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
     return primes
+
+if __name__ == "__main__":
+    # Example test cases
+    print(isWinner(3, [4, 5, 1]))  # Expected: "Ben"
